8b32aec8142ae23139cccf7697dc2bfb11ecc113
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-07 03:21:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "Hi Nana, how are you?"
-  return phrase
+  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "Hi!"
 elsif phrase == "WHAT DID YOU EAT TODAY?"
   return "NO, NOT SINCE 1938!"
