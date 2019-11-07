4faf5b61f80eb845e9671a59bf02a7253c8ccab2
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-07 03:18:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
 if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "Hi Nana, how are you?"
   puts phrase
 elsif phrase == "Hi!"
